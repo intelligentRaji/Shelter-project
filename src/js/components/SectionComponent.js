f8eba513c9dfde90c5a49b15a5c29844d1f3ad59
intelligentRaji <sergdim1234@mail.ru>
2023-03-26 03:52:09 +0200

    feat: almost cmpleted main adaptive

--- src/js/components/SectionComponent.js
+++ src/js/components/SectionComponent.js
@@ -4,9 +4,5 @@ export default class SectionComponent extends BaseComponent {
   constructor(options) {
     super({ tag: "section", ...options });
     this.element.id = options.className;
-    if (options.neighbor) {
-      this.neighbor = document.querySelector(options.neighbor);
-      this.neighbor.after(this.element);
-    }
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,11 @@ async function startApp() {
   const res = await fetch("./json/pets.json");
   const data = await res.json();
   // eslint-disable-next-line no-new
-  new Slider({ className: "our-friends", neighbor: ".about", files: data });
+  new Slider({
+    className: "our-friends",
+    neighbor: document.querySelector(".about"),
+    files: data,
+  });
 }
 
 await startApp();
-
-console.log(
-  "Страница Main 60/60 - все пункты выполнены\n Проверка верстки 7/7\n Вёрстка соответствует макету 35/35\n Требования к css 6/6\n Интерактивность элементов 12/12\n\n Страница Pets 40/40\n Проверка верстки 7/7\n Вёрстка соответствует макету 15/15\n Требования к css 4/4\n Интерактивность элементов 14/14"
-);
